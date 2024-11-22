9dbd4d0e52a8dd3c17441f286e0e2e52ca535d17
Sneha123-zudo <snehamishrasm9@gmail.com>
2024-11-22 08:07:14 +0530

    Neervaani login page

---
+++ login.js
@@ -0,0 +1,11 @@
+// JavaScript to toggle between login and signup forms
+document.getElementById("switchToSignup").addEventListener("click", () => {
+    document.getElementById("loginForm").classList.add("hidden");
+    document.getElementById("signupForm").classList.remove("hidden");
+  });
+  
+  document.getElementById("switchToLogin").addEventListener("click", () => {
+    document.getElementById("signupForm").classList.add("hidden");
+    document.getElementById("loginForm").classList.remove("hidden");
+  });
+  
