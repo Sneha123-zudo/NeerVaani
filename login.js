8c7c3f1e95718577396c9215fc273843d0aac0aa
Sneha123-zudo <snehamishrasm9@gmail.com>
2024-11-24 11:00:31 +0530

    changes

--- login.js
+++ login.js
@@ -1,11 +1,10 @@
 // JavaScript to toggle between login and signup forms
 document.getElementById("switchToSignup").addEventListener("click", () => {
-    document.getElementById("loginForm").classList.add("hidden");
-    document.getElementById("signupForm").classList.remove("hidden");
-  });
-  
-  document.getElementById("switchToLogin").addEventListener("click", () => {
-    document.getElementById("signupForm").classList.add("hidden");
-    document.getElementById("loginForm").classList.remove("hidden");
-  });
-  
+  document.getElementById("loginForm").classList.add("hidden");
+  document.getElementById("signupForm").classList.remove("hidden");
+});
+
+document.getElementById("switchToLogin").addEventListener("click", () => {
+  document.getElementById("signupForm").classList.add("hidden");
+  document.getElementById("loginForm").classList.remove("hidden");
+});
