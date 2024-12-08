cc0c21c8187cc33ff60b89d64e715af543d4e306
Sneha123-zudo <snehamishrasm9@gmail.com>
2024-12-09 00:28:37 +0530

    added the pages

--- slider.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  // Add the 'active' class to all timeline items for the fade-in animation
-  document.querySelectorAll('.timeline-item').forEach((item, index) => {
-      setTimeout(() => {
-          item.style.opacity = '1';
-      }, index * 500); // Delay each event by 500ms
-  });
-};
